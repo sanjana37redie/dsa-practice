86772e2618b91a403fd6f9992e0697f341e198a3
sanjana37redie <120641464+sanjana37redie@users.noreply.github.com>
2026-03-30 20:30:02 +0530

    Create middle_of_list.java

---
+++ LinkedList/middle_of_list.java
@@ -0,0 +1,27 @@
+// Problem: Find Middle of Linked List
+
+class MiddleOfList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode findMiddle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
