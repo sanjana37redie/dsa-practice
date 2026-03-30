edd058177abec4e2cf102cc5beadcd86993f90e9
sanjana37redie <120641464+sanjana37redie@users.noreply.github.com>
2026-03-30 20:17:03 +0530

    Create detect_cycle.java

---
+++ LinkedList/detect_cycle.java
@@ -0,0 +1,33 @@
+// Problem: Detect Cycle in Linked List
+// Check if a cycle exists
+
+class DetectCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
